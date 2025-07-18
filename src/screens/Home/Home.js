92c52d01e3810dad2b0097e4fa41a07f09b1e1ef
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-18 12:07:43 +0530

    Ui changers

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -135,7 +135,7 @@ const styles = StyleSheet.create({
     },
     subtitle: {
         fontSize: 16,
-        color: '#e0e0e0',
+        color: '#e2dbdbff',
         marginTop: 8,
     },
     image: {
